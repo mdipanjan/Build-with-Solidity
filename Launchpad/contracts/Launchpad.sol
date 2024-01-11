b17a8d513bdeb57128e1d7763ce7868b1dead1c6
mdipanjan <mdipanjan33@gmail.com>
2024-01-11 18:32:56 +0530

    state variables

--- Launchpad/contracts/Launchpad.sol
+++ Launchpad/contracts/Launchpad.sol
@@ -1,5 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Launchpad {}
+contract Launchpad is ReentrancyGuard, Pausable, Ownable {
+    using SafeERC20 for IERC20;
+
+    using SafeMath for uint256;
+}
